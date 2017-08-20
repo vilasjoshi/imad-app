7182c51a0efda43cbf67645f21b9cac9797d8b1d
vilasjoshi <vilasjoshi222@gmail.com>
2017-08-20 15:10:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));    
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
     res.send('Article two requested and will be served here');
