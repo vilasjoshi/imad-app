ceb2a79a68a3f1508094b1e6036531ab76180315
vilasjoshi <vilasjoshi222@gmail.com>
2017-08-23 22:22:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
 console.log('Loaded!');
 //move the image
-var imag=document.getElementById('main-text');
-img.onlick = function () {
+var img=document.getElementById('main-text');
+var marginLeft= 0 ;
+function miveRight(){
+    marginLeft= marginLeft+10;
+    img.style.marginLeft=marginLeft + 'px';
     
+}
+img.onlick = function () {
+    var interval= setInterval(moveRight,100);
     img.style.marginLeft='100px';
 };
