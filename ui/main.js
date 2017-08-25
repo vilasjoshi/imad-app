0b48269a97737b059fbce8facc5075c833ba41f2
vilasjoshi <vilasjoshi222@gmail.com>
2017-08-25 12:01:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,10 +24,10 @@ button.onclick = function() {
     
 };
 //Submit nname
-/*var nameInput   =   document.getElementById('name');
+var nameInput   =   document.getElementById('name');
 var name        =   nameInput.value;
-var submit      =   document.getElementById('submit_btn');
-submit.onclick  =   function () {
+var Submit      =   document.getElementById('submit_btn');
+Submit.onclick  =   function () {
   //make request to server and send the name
   
   
@@ -39,4 +39,4 @@ submit.onclick  =   function () {
   }
   var ul        = document.getElementById('namelist');
   ul.innerHTML= list;
-};*/
+};
